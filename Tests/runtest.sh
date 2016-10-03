0db265ca0d742ca650cea9d0a6448d317e65577d
Datamine <contact@johnloeber.com>
2016-10-03 15:27:53 -0700

    jump instructions are correct...

--- Tests/runtest.sh
+++ Tests/runtest.sh
@@ -2,7 +2,7 @@
 
 # $1 is the script, $2 is an optional argument to that script
 echo "reference:"
-python ./reference-interpreters/brainfuck.py $1 $2
+python ./reference-interpreters/brainfuck-simple.py $1 $2
 echo 
 echo "ruby:"
 ruby ../brainfuck-interpreter.rb $1 $2
--- brainfuck-interpreter.rb
+++ brainfuck-interpreter.rb
@@ -4,23 +4,44 @@
 require 'io/console'
 
 class State
-  @@tape_length = 30000
+  @@tape_length = 10
+  attr_accessor :command
   def initialize(script)
     @tape = [0] * @@tape_length
     @data_pointer = 0
     @instruction_pointer = 0
     @script = script
     @script_length = script.length
+    @jumps = precompute_jumps
+  end
+
+  def precompute_jumps
+    stack = []
+    jumphash = {}
+    instruction_pointer = 0
+    while instruction_pointer < @script_length
+      command = @script[instruction_pointer]
+      case command
+      when "["
+        stack << instruction_pointer
+      when "]"
+        target = stack.pop
+        jumphash[target] = instruction_pointer
+        jumphash[instruction_pointer] = target
+      end
+      instruction_pointer += 1
+    end
+    jumphash.keys.each do |k|
+      print k, jumphash[k], "\n"
+    return jumphash
   end
 
   def interpret
     while valid_state do
       command = @script[@instruction_pointer]
-      # nb: a statistical ordered approach to minimize if-statements would be wise
-      # should i use case/switch here?
+      #print "#{@instruction_pointer} #{command} #{@data_pointer} #{@tape}\n"
       case command
       when "+"
-        # think i need to do byte-wise increment, not numerical
         @tape[@data_pointer] += 1
       when "-"
         @tape[@data_pointer] -= 1
@@ -29,49 +50,17 @@ class State
       when "<"
         @data_pointer -= 1
       when "."
-          print @tape[@data_pointer].chr
+        print @tape[@data_pointer].chr
       when ","
-        print "here"
-        input = STDIN.getch
-        input = input.ord
-        puts "++", [input]
-        if input == 26 or input == 13
-          # handle EOF
-          input = 0
-        else
-          input = input
-        end
-        print "here2"
-        @tape[@data_pointer] = input
-        print "here3"
+        @tape[@data_pointer] = STDIN.getch.ord
       when "["
         if @tape[@data_pointer] == 0
-          # skip to the next "]", unless it cannot be found (then break).
-          begin
-            while @script[@instruction_pointer] != "]"
-              @instruction_pointer += 1
-            end
-            #next
-          rescue Exception => e
-            puts e.message
-            puts e.backtrace.inspect
-            exit(false)
-          end
+          @instruction_pointer = @jumps[@instruction_pointer]
         end
       when "]"
-        unless @tape[@data_pointer] == 0
-          # skip to the previous "[", unless it cannot be found (then break).
-          begin
-            while @script[@instruction_pointer] != "["
-              @instruction_pointer -= 1
-            end
-            #next
-          rescue Exception => e
-            puts e.message
-            puts e.backtrace.inspect
-            exit(false)
+        if @tape[@data_pointer] != 0
+          @instruction_pointer = @jumps[@instruction_pointer]
           end
-        end
       end
       @instruction_pointer += 1
     end
@@ -90,7 +79,7 @@ to_interpret = ARGV[0]
 # read the script into memory, strip it of cruft, join the string.
 script_text = IO.readlines(to_interpret).map(&:strip).join(' ')
 script_text.gsub!(brainfuck_not_allowed_regex, "")
+#print script_text
 
-print script_text
 interpreter_instance = State.new(script_text)
 interpreter_instance.interpret
